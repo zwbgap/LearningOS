80d276f8e8928fea4490115fff009a6641e6e52f
ZAM <z2634541542@gmail.com>
2023-09-30 11:05:35 +0800

    creat rust project

---
+++ rustlings/Rust Course/world_hello/target/debug/deps/world_hello-f39ff9ce589cc96c.d
@@ -0,0 +1,5 @@
+D:\26345\Documents\Github\LearningOS\rustlings\Rust Course\world_hello\target\debug\deps\world_hello-f39ff9ce589cc96c.rmeta: src\main.rs
+
+D:\26345\Documents\Github\LearningOS\rustlings\Rust Course\world_hello\target\debug\deps\world_hello-f39ff9ce589cc96c.d: src\main.rs
+
+src\main.rs:
---
+++ rustlings/Rust Course/world_hello/target/debug/world_hello.d
@@ -0,0 +1,1 @@
+D:\26345\Documents\Github\LearningOS\rustlings\Rust\ Course\world_hello\target\debug\world_hello.exe: D:\26345\Documents\Github\LearningOS\rustlings\Rust\ Course\world_hello\src\main.rs
