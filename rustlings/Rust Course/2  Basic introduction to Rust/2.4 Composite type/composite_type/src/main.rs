74cdecd71a4d84453eb94729782b7b61ca007a92
ZAM <z2634541542@gmail.com>
2023-10-07 23:07:56 +0800

    new file

--- rustlings/Rust Course/2  Basic introduction to Rust/2.4 Composite type/composite_type/src/main.rs
+++ rustlings/Rust Course/2  Basic introduction to Rust/2.4 Composite type/composite_type/src/main.rs
@@ -1,3 +1,19 @@
+/*
+ * @Author: ZAM
+ * @Date: 2023-10-05 23:51:16
+ * @LastEditors: zam
+ * @LastEditTime: 2023-10-07 21:52:59
+ * @Description: file content
+ * @FilePath: \LearningOS\rustlings\Rust Course\2  Basic introduction to Rust\2.4 Composite type\composite_type\src\main.rs
+ */
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+}
+
+fn string_test1() {
+    let mut s = String::new();
+    s.push_str("hello");
+    s.push_str(", world!");
+    println!("{}", s);
 }
